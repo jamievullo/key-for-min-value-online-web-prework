fade82d613cf6cb0beabda86cc2841e479b44a41
Jamie Vullo <jvullo@protonmail.com>
2019-04-26 14:51:10 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,6 +7,7 @@ def key_for_min_value(name_hash)
   if name_hash == {}
           return nil
         else
+ 
   min_value = name_hash.first[1]
   min_key = name_hash.first[0]
   
@@ -15,7 +16,7 @@ def key_for_min_value(name_hash)
       if value < min_value   
        min_value = value
        min_key = key
-      else
+      
          
         end
       end  
