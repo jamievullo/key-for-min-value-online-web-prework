0065360b7326229d6af1620652adab7681a9cf87
Jamie Vullo <jvullo@protonmail.com>
2019-04-26 14:44:13 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ require "pry"
 def key_for_min_value(name_hash)
   min_value = name_hash.first[1]
   min_key = name_hash.first[0]
-  binding.pry 
+  
     name_hash.each do |key, value|
                
       if value < min_value   
